66c6753a17726a779b0d9cce93b3ee68b3dfed75
TWaskiw <70579708+TWaskiw@users.noreply.github.com>
2022-12-08 22:27:51 +0100

    Add files via upload

---
+++ Fysio/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Fysioterapi</title>
+    <script src="https://kit.fontawesome.com/491b6e8995.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="navbar">
+    <div class="nav-logo">
+        <h1><span class="logo-span">Mark</span>Skals</h1></div>
+        <div>
+        <ul class="nav-links">
+          <li>Om mig</li>
+          <li>Behandling</li>
+          <li>Kontakt</li>
+        </ul>
+        </div>
+        <div class="nav-button"><href class="log-ind">Login</href><button>Bestil tid        <i class="fa-solid fa-arrow-right"></i></button></div>
+      </nav>
+
---
+++ Fysio/index.php
@@ -0,0 +1,40 @@
+ <?php /*
+session_start();
+    include("mysql.php"); */
+    include_once 'header.php'; 
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Forside</title>
+</head>
+<body>
+    
+<main class="hero-section">
+  <section class="container">
+    <div class="hero-content">
+
+    <div class="hero-text">
+        <h2 class="hero-welcome-text">Fysioterapi med 
+dig i</h2>
+        <h1 class="hero-big">FOKUS</h1>
+        <p class="hero-text-description">Jeg har over 5 års erfaring  med alt fra småskader, sportsskader massage og meget mere.</p>
+        <button>Læs mere <i class="fa-solid fa-arrow-right"></i></button>
+      </div>
+    </div>
+
+  </section>
+</main>
+
+<?php
+/*
+    include_once 'footer.php'; */
+?>
+
+
+
