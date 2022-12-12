1f301fca9490133cefa2d7b731ee07725c96bad3
TWaskiw <70579708+TWaskiw@users.noreply.github.com>
2022-12-12 22:38:31 +0100

    ey

--- Fysio/contact-send.php
+++ Fysio/contact-send.php
@@ -14,6 +14,6 @@
                   $headers .= "Reply-To: $visitor_email \r\n";
                   mail($to,$email_subject,$email_body,$headers);
 
-                  header("Location: index.html");
+                  header("Location: index.php");
 
     ?>
