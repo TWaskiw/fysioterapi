5dc27b61383f2e10222c844f1e7b39929fd5fc1a
TWaskiw <70579708+TWaskiw@users.noreply.github.com>
2022-12-12 22:37:22 +0100

    Kontakt formular

--- Fysio/behandlinger.php
+++ Fysio/behandlinger.php
@@ -1,12 +1,4 @@
-d<!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Fysioterapi</title>
-    <script src="https://kit.fontawesome.com/491b6e8995.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 </head>
 <body>
