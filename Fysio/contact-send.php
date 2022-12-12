86d01925591bb3f402c83885a093842a53dd2bdd
TWaskiw <70579708+TWaskiw@users.noreply.github.com>
2022-12-12 22:41:35 +0100

    Contact

--- Fysio/contact-send.php
+++ Fysio/contact-send.php
@@ -9,7 +9,7 @@
                   "Email: $email\n".
                   "Besked: $message\n";
 
-                  $to = "thomaswpedersen@hotmail.com";
+                  $to = "runelehtonen98@gmail.com";
                   $headers = "From: $email_from \r\n";
                   $headers .= "Reply-To: $visitor_email \r\n";
                   mail($to,$email_subject,$email_body,$headers);
