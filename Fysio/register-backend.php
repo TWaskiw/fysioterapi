c54db5396a8b058b55f7a55bbcb2f57f8ae1ae45
TWaskiw <70579708+TWaskiw@users.noreply.github.com>
2022-12-12 12:58:13 +0100

    Hej

---
+++ Fysio/register-backend.php
@@ -0,0 +1,71 @@
+<?php
+    include("mysql.php");
+    session_start();
+
+    if(isset($_POST['submit'])) {
+
+        $firstname = $_REQUEST["firstname"];
+        $lastname = $_REQUEST["lastname"];
+        $number = $_REQUEST["number"];
+        $email = $_REQUEST["email"];
+        $gender = $_REQUEST["gender"];
+        $password = $_REQUEST["password"];
+        $passwordRepeat = $_REQUEST["passwordRepeat"];
+
+        if(isset($_POST['firstname']) !=""){
+        $_SESSION['firstname'] = $_POST['firstname'];}
+
+        if(isset($_POST['lastname']) !=""){
+        $_SESSION['lastname'] = $_POST['lastname'];}
+
+        if(isset($_POST['number']) !=""){
+        $_SESSION['number'] = $_POST['number'];}
+
+        if(isset($_POST['email']) !=""){
+        $_SESSION['email'] = $_POST['email'];}
+
+
+        $sql_user = "SELECT * FROM userPass WHERE mobilenumber='$number'";
+        $res_user = mysqli_query($mySQL, $sql_user) or die(mysqli_error($mySQL));
+
+        if(empty($firstname) || empty($lastname) || empty($number) || empty($email) || empty($gender) || empty($password)){
+            header('location: register.php?error=emptyField');
+            exit();
+        }
+        
+        if (mysqli_num_rows($res_user) > 0 ) {
+            $_SESSION['mobilenumber'] = "";
+            header('location: register.php?error=userTaken');
+            exit();
+        }
+        
+        if($password !== $passwordRepeat){
+            header('location: register.php?error=wrongPassword');
+            exit();
+        }
+
+         else {
+            $newUser = "INSERT INTO user (firstname, lastname, number, email, gender)
+            VALUES ('$firstname','$lastname','$number','$email','$gender')";
+            $mySQLfind = $mySQL->query($newUser);
+
+            $findUser = "SELECT id FROM user ORDER BY id DESC LIMIT 1";
+            $response = $mySQL->query($findUser);
+            $data = $response->fetch_object();
+            
+            $passwordHash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+            $signupPass = "INSERT INTO userPass (id, mobilenumber, password) 
+            VALUES ('$data->id','$mobilenumber', '$passwordHash')";
+            $response = $mySQL->query($signupPass);
+
+            unset($_SESSION['firstname']);
+            unset($_SESSION['lastname']);
+            unset($_SESSION['number']);
+            unset($_SESSION['email']);
+            unset($_SESSION['mobilenumber']);
+
+            header('location: register.php?error=none');
+            }
+        }
+
+?>
---
+++ Fysio/register.php
@@ -0,0 +1,72 @@
+<?php 
+    include("mysql.php");
+    session_start();
+    /* include_once 'header.php';
+     if(isset($_SESSION['id'])) {
+    header('location: welcome.php?status=loggedin');
+    exit; 
+    } */
+
+
+?>
+
+<html>
+<body>
+
+<form class="form-register" action="register-backend.php" name="register" method="post">
+    <div class="form-form-register">
+    <h2>Opret bruger</h2>
+        <div class="form-flex">
+            <div class="register-info">
+                <label for="firstname">Fornavn</label>
+                <input type="text" name="firstname" <?php if(isset($_SESSION['firstname'])!=""){ 
+                    echo " value='".$_SESSION['firstname']."'"; }?>
+                ><br>
+
+                <label for="lastname">Efternavn</label>
+                <input type="text" name="lastname" <?php if(isset($_SESSION['lastname'])!=""){ 
+                    echo " value='".$_SESSION['lastname']."'"; }?>><br>
+
+                <label for="number">Telefon nummer</label>
+                <input type="number" name="number" <?php if(isset($_SESSION['number'])!=""){ 
+                    echo " value='".$_SESSION['number']."'"; }?>><br>
+
+                <label for="email">E-Mail</label>
+                <input type="text" name="email" <?php if(isset($_SESSION['email'])!=""){ 
+                    echo " value='".$_SESSION['email']."'"; }?>><br>
+
+                <label for="gender">Køn</label>
+                <select type="text" name="gender">
+                    <option value="mand">Mand</option>
+                    <option value="kvinde">Kvinde</option>
+                </select><br>
+            </div>
+            <div class="register-login">
+                    <label for="password">Kodeord</label>
+                    <input type="password" name="password"><br>
+
+                    <label for="passwordRepeat">Gentag kodeord</label>
+                    <input type="password" name="passwordRepeat"><br>
+                    <button type="submit" name="submit">Tilmeld</button>
+                    <?php 
+                        if (isset($_GET["error"])) {
+                            if ($_GET["error"] == "emptyField") {
+                                echo "<p class='register-error'>Udfyld alle felter </p>";
+                            }
+                            else if ($_GET["error"] == "userTaken") {
+                                echo "<p class='register-error'>Beklemailr, brugernavn er allerede temailt </p>";
+                            } 
+                            else if ($_GET["error"] == "wrongPassword") {
+                                echo "<p class='register-error'>Kodeord stemmer ikke overens </p>";
+                            }
+                            else if ($_GET["error"] == "none") {
+                                echo "<p class='register-success'>Du er nu registreret! Gå til <a href='login.php'>Login</a></p>";
+                            }
+                        }
+                    include_once 'footer.php';
+                    ?>
+            </div>
+        </div>
+    </div>
+</form>
+
