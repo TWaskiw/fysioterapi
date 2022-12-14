937eb990ee28cfc0c8dbbc1b2af79ede48f57d14
runelehtonen <71000750+runelehtonen@users.noreply.github.com>
2022-12-14 21:40:15 +0100

    Add files via upload

--- Fysio/header.php
+++ Fysio/header.php
@@ -6,27 +6,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <title>Fysioterapi</title>
-    <script src="https://kit.fontawesome.com/491b6e8995.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
     <script src="script.js"></script>
 </head>
 <body>
-<nav  class="navbar ">
-    <div class="nav-logo">
-        <h1 href="index.php"><span class="logo-span">Mark</span>Skals</h1></div>
-        <div>
-        <ul class="nav-links">
-          <li>Om mig</li>
-          <li>Behandling</li>
-          <li>Kontakt</li>
-        </ul>
-        </div>
-        <?php 
-            if(isset($_SESSION['number'])) {
-              echo "<div class='nav-button'><a href='profil.php' class='log-ind'>Min side</a><button class='default-button'>Bestil tid        <i class='fa-solid fa-arrow-right'></i></button></div>";
-              echo "<li><a href='logout.php'>Log ud</a></li>'";
-            } else {
-                echo "<div class='nav-button'><a href='login.php' class='log-ind'>Login</a><button class='default-button'>Bestil tid        <i class='fa-solid fa-arrow-right'></i></button></div>";
-            }
-            ?>
-      </nav>
+
+<nav>
+    <ul class="menu">
+    <?php 
+        if(isset($_SESSION['number'])) {
+            echo "<li class='navlogo'><a href='#'><span>Mark</span>Skals</a></li>";
+            echo "<li class='item'><a href='#'>Behandlinger</a></li>";
+            echo "<li class='item'><a href='#'>Om mig</a></li>";
+            echo "<li class='item'><a href='#'>Kontakt</a></li>";
+            echo "<li class='item'><a href='profil.php'>Min side</a></li>";
+            echo "<li class='item'><a href='logout.php'>Log ud</a></li>";
+            echo "<li class='item button'><a href='kalender.php'>Bestil tid</a></li>";
+        } else {
+            echo "<li class='navlogo'><a href='#'><span>Mark</span>Skals</a></li>";
+            echo "<li class='item'><a href='#'>Behandlinger</a></li>";
+            echo "<li class='item'><a href='#'>Om mig</a></li>";
+            echo "<li class='item'><a href='#'>Kontakt</a></li>";
+            echo "<li class='item'><a href='login.php'>Login</a></li>";
+            echo "<li class='item button'><a href='kalender.php'>Bestil tid</a></li>";
+        }
+    ?>
+
+        <li class="toggle"><a href="#"><i class="fas fa-bars"></i></a></li>
+    </ul>
+</nav>
+
+<script>
+    const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+ 
+/* Toggle mobile menu */
+function toggleMenu() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+         
+        // adds the menu (hamburger) icon
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+    } else {
+        menu.classList.add("active");
+         
+        // adds the close (x) icon
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+    }
+}
+ 
+/* Event Listener */
+toggle.addEventListener("click", toggleMenu, false);
+</script>
+
 
