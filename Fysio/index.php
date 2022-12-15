cab917a7043c23cf249dcf5d3b680c776be67863
TWaskiw <70579708+TWaskiw@users.noreply.github.com>
2022-12-15 21:39:16 +0100

    Virkede ikke

--- Fysio/index.php
+++ Fysio/index.php
@@ -1,34 +1,10 @@
  <?php
     session_start(); 
     include("mysql.php");
+    include_once 'components/header.php'; 
 ?> 
 
 <body>
-
-<nav>
-    <ul class="menu">
-    <?php 
-        if(isset($_SESSION['number'])) {
-            echo "<li class='navlogo'><a href='index.php'><span>Mark</span>Skals</a></li>";
-            echo "<li class='item'><a href='index.php#anchor-behandlinger'>Behandlinger</a></li>";
-            echo "<li class='item'><a href='index.php#anchor-about'>Om mig</a></li>";
-            echo "<li class='item'><a href='index.php#anchor-contact'>Kontakt</a></li>";
-            echo "<li class='item'><a href='profil.php'>Min side</a></li>";
-            echo "<li class='item'><a href='logout.php'>Log ud</a></li>";
-            echo "<li class='item button'><a href='kalender.php'>Bestil tid</a></li>";
-        } else {
-            echo "<li class='navlogo'><a href='index.php'><span>Mark</span>Skals</a></li>";
-            echo "<li class='item'><a href='index.php#anchor-behandlinger'>Behandlinger</a></li>";
-            echo "<li class='item'><a href='index.php#anchor-about'>Om mig</a></li>";
-            echo "<li class='item'><a href='index.php#anchor-contact'>Kontakt</a></li>";
-            echo "<li class='item'><a href='login.php'>Login</a></li>";
-            echo "<li class='item button'><a href='kalender.php'>Bestil tid</a></li>";
-        }
-    ?>
-
-        <li class="toggle"><a href="#"><i class="fas fa-bars"></i></a></li>
-    </ul>
-</nav>
 
     
 <main class="hero-section">
